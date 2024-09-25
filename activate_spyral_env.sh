e7c738eb61d074ab03af07a9e12d71d05fc7b6ea
gwm17 <gordonmccann215@gmail.com>
2024-09-25 13:38:39 -0400

    Use our env file to set the dragon directory. Setup basic error handling on dragon path.

--- activate_spyral_env.sh
+++ activate_spyral_env.sh
@@ -1,14 +1,25 @@
 #!/bin/bash
 
-DRAGON_DIR=$1
+source .env
+if [ ! -d "$DRAGON_DIR" ]
+then
+    echo "The DRAGON_DIR environment variable is not set, or is invalid"
+    echo "Please set this variable using the .env file included with this directory"
+    echo "Current value: ${DRAGON_DIR}"
+    return
+fi
 MODULE_DIR=$DRAGON_DIR/modulefiles
 
 echo "Activating Spyral virtual environment .venv and Dragon"
 
-module use /soft/modulefiles
+echo "Loading conda to get Python..."
+module use /soft/modulefiles &> /dev/null
 module load conda
+echo "Activating conda base environment..."
 conda activate base
+echo "Activating Spyral virtual environment..."
 source .venv/bin/activate
+echo "Loading Dragon modules..."
 module use $MODULE_DIR
 module load dragon
 
--- create_spyral_env.sh
+++ create_spyral_env.sh
@@ -1,20 +1,33 @@
 #!/bin/bash
 
-DRAGON_DIR=$1
+source .env
+if [ ! -d "$DRAGON_DIR" ]
+then
+    echo "The DRAGON_DIR environment variable is not set, or is invalid"
+    echo "Please set this variable using the .env file included with this directory"
+    echo "Current value: ${DRAGON_DIR}"
+    return
+fi
 DRAGON_WHEEL=$(ls $DRAGON_DIR/dragon*.whl)
 PYCAPNP_WHEEL=$(ls $DRAGON_DIR/pycapnp*.whl)
 MODULE_DIR=$DRAGON_DIR/modulefiles
 
 echo "Preparing a Spyral environment with Dragon from ${DRAGON_DIR}"
 
-module use /soft/modulefiles
+echo "Loading conda to get Python..."
+module use /soft/modulefiles &> /dev/null
 module load conda
+echo "Activating conda base environment..."
 conda activate base
+echo "Creating a new virtual environment..."
 python -m venv .venv
 source .venv/bin/activate
+echo "Installing Dragon specific wheels..."
 pip install $PYCAPNP_WHEEL
 pip install $DRAGON_WHEEL
+echo "Installing Spyral and dependencies..."
 pip install attpc_spyral
+echo "Loading Dragon modules..."
 module use $MODULE_DIR
 module load dragon
 
